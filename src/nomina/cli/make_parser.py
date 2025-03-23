6c2c6b806c3d0867307b7dc6e03df8addff35664
Andy Shahidehpour <anshahidehpour@gmail.com>
2025-03-23 14:13:41 -0500

    Removed pypi option for parser since it's all pypi for now

--- src/nomina/cli/make_parser.py
+++ src/nomina/cli/make_parser.py
@@ -7,13 +7,5 @@ def make_parser():
     )
 
     parser.add_argument("name", type=str, help="Candidate package name.", nargs="+")
-    parser.add_argument(
-        "-env",
-        "--environment",
-        type=lambda s: s.lower(),
-        help="The environment to use.",
-        default="pypi",
-        choices=["pypi"],
-    )
 
     return parser
